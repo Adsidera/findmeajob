140fa9c624d601235a190dae0fa040ef6b82975a
Adsidera <annamaria.costalonga@gmail.com>
2020-09-16 11:27:00 +0200

    refactored and added fixture

--- test/findmeajob/parsers/remote_jobs_test.exs
+++ test/findmeajob/parsers/remote_jobs_test.exs
@@ -7,7 +7,9 @@ defmodule Findmeajob.Parsers.RemoteJobsTest do
     scraped_list = RemoteJobs.parse(response.body)
     job = List.first(scraped_list)
     {:ok, link} = Map.fetch(job, :link)
+
     assert Enum.count(scraped_list) != 0
     assert link =~ "https://remoteok.io"
+    refute Enum.find(scraped_list, fn job -> job[:added_on] == '1yr' end)
   end
 end
